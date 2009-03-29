101701fb1597ecdf387e8770c54c247b18a782a7
Josh French <josh@vitamin-j.com>
2009-03-29 10:52:05 -0400

    Smaller/safer sitemeta accessor

--- lib/wp_site.rb
+++ lib/wp_site.rb
@@ -2,16 +2,12 @@ class WPSite < ActiveRecord::Base
   set_table_name :wp_site
   has_many :wp_blogs
   has_many :sitemeta, :class_name => 'WPSitemeta', :foreign_key => :site_id do
-    # Let ActiveRecord try to match a dynamic method first so we don't ever
-    # step on any finders. Next, try to look up a matching record. If all else
-    # fails, re-raise the original NoMethodError.
-    def method_missing(method, *args, &block)
-      super
-    rescue NoMethodError => e
-      if meta = find_by_meta_key(method.to_s) 
-        meta.meta_value
+    def [](meta)
+      case meta
+      when String, Symbol
+        find_by_meta_key(meta.to_s)
       else
-        raise e
+        super
       end
     end
   end
