c082a3e232b5ef558bfb0e0046b2fb0228b39dda
Josh French <josh@vitamin-j.com>
2009-04-28 00:17:46 -0400

    User predicates

--- lib/user.rb
+++ lib/user.rb
@@ -4,4 +4,12 @@ class User < ActiveRecord::Base
   has_many :links, :foreign_key => :link_owner, :extend => LinkExtension
   has_many :posts, :foreign_key => :post_author, :extend => PostExtension
   has_many :comments, :extend => CommentExtension
+
+  def approved?
+    user_status == '1'
+  end
+
+  def not_approved?
+    user_status == '0'
+  end
 end
