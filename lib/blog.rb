19f21af9f3ce1dbfe9e361bf2b552ee247eb5561
Josh French <josh@vitamin-j.com>
2009-04-27 22:43:07 -0400

    Missing associations

--- lib/blog.rb
+++ lib/blog.rb
@@ -2,6 +2,16 @@ class Blog < ActiveRecord::Base
   self.primary_key = :blog_id
   has_many :registration_logs
   belongs_to :site, :foreign_key => :site_id
-  has_many :options
   has_many :links
+  has_many :posts
+  has_many :options do
+    def [](option)
+      case option
+      when String, Symbol
+        find_by_option_name(option.to_s)
+      else
+        super
+      end
+    end
+  end
 end
--- lib/user.rb
+++ lib/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
   self.primary_key = :ID
   has_many :usermeta, :class_name => 'Usermeta', :foreign_key => :user_id, :extend => MuFu::MetaExtension
   has_many :links, :foreign_key => :link_owner
+  has_many :posts, :foreign_key => :post_author
 end
